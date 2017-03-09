8619a9f119793cd0f79cb2dfb2b70c546f2a3f38
facoronel93 <queseyo.fede@gmail.com>
2017-03-09 10:43:28 -0300

    metodo diferenciaEntrePalabras

--- PalabraMasLarga/PalabraMasLarga/Program.cs
+++ PalabraMasLarga/PalabraMasLarga/Program.cs
@@ -10,16 +10,28 @@ namespace PalabraMasLarga
     {
         static void cargaDatos(string palabra1, string palabra2)
         {
-            
             Console.WriteLine("ingrese palabra 1");
             palabra1 = Console.ReadLine();
             Console.WriteLine("ingrese palabra 2");
             palabra2 = Console.ReadLine();
         }
+        static void showDiferenciaEntrePalabras(string palabra1, string palabra2)
+        {
+            int diferencia;
+            diferencia = (palabra1.Length - palabra2.Length);
+            if(palabra1.Length > palabra2.Length)
+            {
+                Console.WriteLine($"la palabra {palabra1} tiene {diferencia} letras mas que {palabra2}");
+            }
+            else
+                Console.WriteLine($"la palabra {palabra2} tiene {diferencia} letras mas que {palabra1}");
+        }
         static void Main(string[] args)
         {
             string palabra1=null, palabra2=null;
             cargaDatos(palabra1, palabra2);
+            showDiferenciaEntrePalabras(palabra1, palabra2);
+            Console.ReadKey();
         }
     }
 }
