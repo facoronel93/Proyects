b6d3b276448fd434bed9104b869a7c5f59fe8eb5
lekuak <fa.coronel@hormail.com>
2017-04-18 23:56:43 -0300

    act

--- Proyect/Proyect.Droid/Resources/Resource.Designer.cs
+++ Proyect/Proyect.Droid/Resources/Resource.Designer.cs
@@ -26,10 +26,52 @@ namespace Proyect.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxBind = global::Proyect.Droid.Resource.Attribute.MvxBind;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxDropDownItemTemplate = global::Proyect.Droid.Resource.Attribute.MvxDropDownItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxGroupItemTemplate = global::Proyect.Droid.Resource.Attribute.MvxGroupItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxItemTemplate = global::Proyect.Droid.Resource.Attribute.MvxItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxLang = global::Proyect.Droid.Resource.Attribute.MvxLang;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxSource = global::Proyect.Droid.Resource.Attribute.MvxSource;
+			global::MvvmCross.Binding.Droid.Resource.Attribute.MvxTemplate = global::Proyect.Droid.Resource.Attribute.MvxTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Id.MvvmCrossTagId = global::Proyect.Droid.Resource.Id.MvvmCrossTagId;
+			global::MvvmCross.Binding.Droid.Resource.Id.MvxBindingTagUnique = global::Proyect.Droid.Resource.Id.MvxBindingTagUnique;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxBinding = global::Proyect.Droid.Resource.Styleable.MvxBinding;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxBinding_MvxBind = global::Proyect.Droid.Resource.Styleable.MvxBinding_MvxBind;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxBinding_MvxLang = global::Proyect.Droid.Resource.Styleable.MvxBinding_MvxLang;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxControl = global::Proyect.Droid.Resource.Styleable.MvxControl;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxControl_MvxTemplate = global::Proyect.Droid.Resource.Styleable.MvxControl_MvxTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxExpandableListView = global::Proyect.Droid.Resource.Styleable.MvxExpandableListView;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxExpandableListView_MvxGroupItemTemplate = global::Proyect.Droid.Resource.Styleable.MvxExpandableListView_MvxGroupItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxImageView = global::Proyect.Droid.Resource.Styleable.MvxImageView;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxImageView_MvxSource = global::Proyect.Droid.Resource.Styleable.MvxImageView_MvxSource;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxListView = global::Proyect.Droid.Resource.Styleable.MvxListView;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxListView_MvxDropDownItemTemplate = global::Proyect.Droid.Resource.Styleable.MvxListView_MvxDropDownItemTemplate;
+			global::MvvmCross.Binding.Droid.Resource.Styleable.MvxListView_MvxItemTemplate = global::Proyect.Droid.Resource.Styleable.MvxListView_MvxItemTemplate;
 		}
 		
 		public partial class Attribute
 		{
+			
+			// aapt resource value: 0x7f010000
+			public const int MvxBind = 2130771968;
+			
+			// aapt resource value: 0x7f010004
+			public const int MvxDropDownItemTemplate = 2130771972;
+			
+			// aapt resource value: 0x7f010005
+			public const int MvxGroupItemTemplate = 2130771973;
+			
+			// aapt resource value: 0x7f010003
+			public const int MvxItemTemplate = 2130771971;
+			
+			// aapt resource value: 0x7f010001
+			public const int MvxLang = 2130771969;
+			
+			// aapt resource value: 0x7f010006
+			public const int MvxSource = 2130771974;
+			
+			// aapt resource value: 0x7f010002
+			public const int MvxTemplate = 2130771970;
 			
 			static Attribute()
 			{
@@ -57,11 +99,81 @@ namespace Proyect.Droid
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040004
+			public const int Ciudad = 2130968580;
+			
+			// aapt resource value: 0x7f040000
+			public const int MvvmCrossTagId = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int MvxBindingTagUnique = 2130968577;
+			
+			// aapt resource value: 0x7f040011
+			public const int condLabel = 2130968593;
+			
+			// aapt resource value: 0x7f040010
+			public const int condSection = 2130968592;
+			
+			// aapt resource value: 0x7f040005
+			public const int getSection = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int getWeatherButton = 2130968582;
+			
+			// aapt resource value: 0x7f04000e
+			public const int humidLabel = 2130968590;
+			
+			// aapt resource value: 0x7f04000d
+			public const int humidSection = 2130968589;
+			
+			// aapt resource value: 0x7f04000f
+			public const int humidText = 2130968591;
+			
+			// aapt resource value: 0x7f040002
+			public const int latSection = 2130968578;
+			
+			// aapt resource value: 0x7f040008
+			public const int locLabel = 2130968584;
+			
+			// aapt resource value: 0x7f040007
+			public const int locSection = 2130968583;
+			
+			// aapt resource value: 0x7f040009
+			public const int locationText = 2130968585;
+			
+			// aapt resource value: 0x7f040003
+			public const int longLabel = 2130968579;
+			
+			// aapt resource value: 0x7f040012
+			public const int pais = 2130968594;
+			
+			// aapt resource value: 0x7f04000b
+			public const int tempLabel = 2130968587;
+			
+			// aapt resource value: 0x7f04000a
+			public const int tempSection = 2130968586;
+			
+			// aapt resource value: 0x7f04000c
+			public const int tempText = 2130968588;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int clima = 2130903040;
 			
 			static Layout()
 			{
@@ -76,11 +188,11 @@ namespace Proyect.Droid
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
 			
 			static String()
 			{
@@ -88,6 +200,57 @@ namespace Proyect.Droid
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] MvxBinding = new int[] {
+					2130771968,
+					2130771969};
+			
+			// aapt resource value: 0
+			public const int MvxBinding_MvxBind = 0;
+			
+			// aapt resource value: 1
+			public const int MvxBinding_MvxLang = 1;
+			
+			public static int[] MvxControl = new int[] {
+					2130771970};
+			
+			// aapt resource value: 0
+			public const int MvxControl_MvxTemplate = 0;
+			
+			public static int[] MvxExpandableListView = new int[] {
+					2130771973};
+			
+			// aapt resource value: 0
+			public const int MvxExpandableListView_MvxGroupItemTemplate = 0;
+			
+			public static int[] MvxImageView = new int[] {
+					2130771974};
+			
+			// aapt resource value: 0
+			public const int MvxImageView_MvxSource = 0;
+			
+			public static int[] MvxListView = new int[] {
+					2130771971,
+					2130771972};
+			
+			// aapt resource value: 1
+			public const int MvxListView_MvxDropDownItemTemplate = 1;
+			
+			// aapt resource value: 0
+			public const int MvxListView_MvxItemTemplate = 0;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
---
+++ Proyect/Proyect.Droid/climaView.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using MvvmCross.Droid.Views;
+
+namespace Proyect.Droid
+{
+    [Activity(Label = "Clima", MainLauncher = true, Icon = "@drawable/icon")]
+    public class climaView : MvxActivity
+    {
+        public new core.ViewModels.viewModelClima ViewModel
+        {
+            get { return (core.ViewModels.viewModelClima)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
+        protected override void OnViewModelSet()
+        {
+            base.OnViewModelSet();
+            SetContentView(Resource.Layout.clima);
+        }
+  
+    }
+}
+
